678a29a96c97342dbf5e38cd03a0b98e327eb7e8
permacode y <ryudu26@hotmail.fr>
2024-06-06 14:34:10 +0000

    updated PictureCrudController.php

--- src/Controller/Admin/PictureCrudController.php
+++ src/Controller/Admin/PictureCrudController.php
@@ -3,8 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Picture;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +20,34 @@ class PictureCrudController extends AbstractCrudController
         return Picture::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')
+                ->hideOnForm(),
+            TextField::new('filename'),
             TextEditorField::new('description'),
+            ImageField::new('filename', 'description')
+                ->hideOnForm(),
+            AssociationField::new('user'),
+            DateField::new('createdAt')
+                ->setLabel("Created at"),
+            DateField::new('Updated at')
+                ->setLabel("Updated at"),
         ];
     }
-    */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Pictures')
+            ->setEntityLabelInSingular('Picture')
+            ->setSearchFields(['user', 'filename'])
+            ->setDefaultSort(['user' => 'ASC']);
+    }
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('filename');
+    }
 }
